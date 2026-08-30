f9c98d3d8f4f5a28751596e3ffeea550cdcf984f
Ali Al Yahya <20233842+ayx234@users.noreply.github.com>
2026-08-30 19:44:52 -0400

    feat: handle symbol argument in element.sh

--- element.sh
+++ element.sh
@@ -22,6 +22,24 @@ ELEMENT() {
       BOILING_POINT=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
       echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
     fi
+  elif [[ $1 =~ ^[A-Za-z]+$ ]]
+  # if text provided as argument
+  then
+    # if it is >= 2 characters
+    if [[ ${#1} -le 2 ]]
+    then
+      ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE symbol = '$1'")
+      if [[ ! -z "$ATOMIC_NUMBER" ]]
+      then
+        NAME="$($PSQL "SELECT name FROM elements WHERE atomic_number = $ATOMIC_NUMBER")"
+        SYMBOL="$($PSQL "SELECT symbol FROM elements WHERE atomic_number = $ATOMIC_NUMBER")"
+        TYPE="$($PSQL "SELECT types.type FROM types RIGHT JOIN properties USING(type_id) WHERE atomic_number = $ATOMIC_NUMBER")"
+        MASS=$($PSQL "SELECT atomic_mass from properties WHERE atomic_number = $ATOMIC_NUMBER")
+        MELTING_POINT=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
+        BOILING_POINT=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
+        echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+      fi
+    fi
   fi
 }
 
