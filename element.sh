fe7ea6849dc2e9d22d2d4db291ac9564095d8491
Ali Al Yahya <20233842+ayx234@users.noreply.github.com>
2026-08-30 18:28:08 -0400

    feat: handle no argument in element.sh

--- element.sh
+++ element.sh
@@ -2,3 +2,11 @@
 
 PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
 
+ELEMENT() {
+  if [[ -z "$1" ]]
+  then
+    echo Please provide an element as an argument.
+  fi
+}
+
+ELEMENT "$1"
